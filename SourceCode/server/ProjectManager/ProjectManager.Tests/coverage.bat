1bfd3d7b8d9986e755d3dcea99b8047597771a24
sagarendrapaul <50829246+sagarendrapaul@users.noreply.github.com>
2019-05-22 08:35:46 -0700

    coverage.bat file updated

--- SourceCode/server/ProjectManager/ProjectManager.Tests/coverage.bat
+++ SourceCode/server/ProjectManager/ProjectManager.Tests/coverage.bat
@@ -1,2 +1,1 @@
-
-C:\SagarPaul_485088\FSE_SBA\SourceCode\server\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\SagarPaul_485088\FSE_SBA\SourceCode\server\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
+ReportGenerator.exe -reports:"C:\Users\Administrator\Desktop\FSE_SBA_ProjectManager-master\SourceCode\server\ProjectManager\packages\OpenCover.4.6.519\tools\results.xml" -targetdir:"C:\Users\Administrator\Desktop\FSE_SBA_ProjectManager-master\codeCoverageReport"
